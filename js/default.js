ba33d91f251ec364555bd67ce6e7a58071746630
lee <dnr2608@naver.com>
2023-01-09 00:11:52 +0900

    4

--- js/default.js
+++ js/default.js
@@ -1,4 +1,6 @@
-$(function(){
+ $(function(){
+
+  $(".includedHeader").load("header.html"); 
 
   $('.btn_menu').on('click', function(){
     $(this).toggleClass('active');
@@ -15,4 +17,37 @@ $(function(){
       $(this).removeClass('active');
     }
   })
+  var img = $('.detail_info .list img');
+  
+  img.on('click', imgInfo);
+  
+  function imgInfo(){
+    var src = $(this).attr('src');
+    var alt = $(this).attr('alt');
+    var thoumb = $('.tuoumbnail');
+  
+    thoumb.attr('src', src).attr('alt', alt);
+  }
+
+  $(window).on('resize load', resize);
+
+  function resize() {
+    var winWidth = $(window).width();
+    if( winWidth < 1199) {
+      var img = $('.tuoumbnail');
+      var li = $('.detail_info .list li');
+      var imgWid = img.width();
+      var imgHig = imgWid / 2;
+
+      img.css('height', imgWid);
+      li.find('img').css('height', imgHig - 4.225);
+      $('.detail_info .more span').removeClass('hide');
+    }else {
+      $('.tuoumbnail').removeAttr('style');
+      $('.detail_info .list li img').removeAttr('style');
+      $('.detail_info .more span').addClass('hide')
+    }
+  }
+
 })
+
